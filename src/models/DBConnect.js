dfd084c63f48bbb0121a9c8f6d71bee652bde7ee
Mihail <mohonovproduction@yandex.ru>
2021-12-03 02:37:27 +0300

    zero password

--- src/models/DBConnect.js
+++ src/models/DBConnect.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
-    password: process.env.PASSWORD,
+    password: '',
     database: 'testdb'
 })
 
