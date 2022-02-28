916a2d4fcac3c3757a36172908080b3914a94b24
Mihail <mohonovproduction@yandex.ru>
2022-02-28 14:38:41 +0300

    remove admin.scan

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -2,21 +2,6 @@ const { Scenes: { WizardScene } } = require('telegraf')
 
 const Admin = {}
 
-Admin.scan = function(ctx) {
-
-	let chat = JSON.stringify(ctx.message.chat)
-	console.log(chat)
-	chat = '_User data:_ \n' + chat + '\n Backend output \n [/console.log()](/console)'
-	chat = chat.split(/{/).join('{\n').split(/}/).join('\n}').split(/,/).join(',\n').split(/:/).join(': ')
-
-	let user = JSON.stringify(ctx.message.from)
-	console.log(user)
-	user = '_Chat data:_ \n' + user + '\n Backend output \n [/console.log()](/console)'
-	user = user.split(/{/).join('{\n').split(/}/).join('\n}').split(/,/).join(',\n').split(/:/).join(': ')
-
-	return user + ' \n\n ' + chat
-}
-
 Admin.pinMessage = function(ctx) {
 	console.log(ctx.message)
 
